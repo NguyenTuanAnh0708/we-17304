40b89d7a157734476f8289133b2fda05f546db33
nguyentuananh <nguyentuananhfedev@gmail.com>
2023-07-09 10:56:07 +0700

    complet lab1

--- src/components/Lab1/index.tsx
+++ src/components/Lab1/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Search from "./Search";
+import Time from "./Time";
 
 const Lab1: React.FC = () => {
   return (
     <div>
       <h1>Lab1</h1>
       <Search />
+      <Time />
     </div>
   );
 };
