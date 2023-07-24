e1c7c327673b7a98eea62ee07602dd2090243026
nguyentuananh <nguyentuananhfedev@gmail.com>
2023-07-24 23:40:43 +0700

    fix custoom hook

---
+++ src/components/Lab5/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Lab5 = () => {
+  return <div>Lab5</div>;
+};
+
+export default Lab5;
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,10 +2,10 @@ import { useEffect, useRef, useState } from "react";
 
 const useDebounce = (value: string, delay = 500) => {
   const [debouncedValue, setDebouncedValue] = useState("");
-  const timerRef = useRef<number | undefined>();
+  const timerRef = useRef<number | undefined>(undefined);
 
   useEffect(() => {
-    timerRef.current = setTimeout(() => setDebouncedValue(value), delay);
+    timerRef.current = window.setTimeout(() => setDebouncedValue(value), delay);
     return () => {
       clearTimeout(timerRef.current);
     };
